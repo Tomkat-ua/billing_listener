7ddc2f7dad8219cdaebcf588a4eee4a4bc270186
Kostevych Viacheslav <tomkat.s@gmail.com>
2026-05-19 09:40:27 +0300

    ref 67

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-import logging,requests,time,pymysql
+import logging,pymysql
 import config as c
 from flask import Flask, request
 
 app = Flask(__name__)
 
-
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Секрети (локально беруться дефолти, в Docker прилетять з Infisical)
-X_TOKEN = c.params.get('bill_webhook_token')
-
-# Зовнішня адреса вашого сервера (наприклад, через Cloudflare Tunnel чи білий IP)
+# Зовнішня адреса
 WEBHOOK_URL = c.params.get('bill_webhook_url')
 WEBHOOK_ROUTE = WEBHOOK_URL.rsplit('/', 1)[-1]
 
@@ -25,24 +21,24 @@ DB_CONFIG = {
 }
 
 
-def register_webhook():
-    """Функція автоматичної реєстрації нашого URL в Monobank"""
-    # Даємо серверу Uvicorn 2 секунди, щоб повністю піднятися
-    time.sleep(2)
-
-    url = c.params.get('MONO_WEBHOOK_URL')
-    headers = {"X-Token": X_TOKEN}
-    payload = {"webHookUrl": WEBHOOK_URL}
-
-    app.logger.info(f"Спроба реєстрації вебхука в Monobank на адресу: {WEBHOOK_URL}")
-    try:
-        response = requests.post(url, headers=headers, json=payload, timeout=10)
-        if response.status_code == 200:
-            app.logger.info("Успішно: Monobank підтвердив реєстрацію вебхука!")
-        else:
-            app.logger.error(f"Помилка реєстрації вебхука: {response.status_code} - {response.text}")
-    except Exception as e:
-        app.logger.error(f"Не вдалося зв'язатися з API Monobank: {e}")
+# def register_webhook():
+#     """Функція автоматичної реєстрації нашого URL в Monobank"""
+#     # Даємо серверу Uvicorn 2 секунди, щоб повністю піднятися
+#     time.sleep(2)
+#
+#     url = c.params.get('MONO_WEBHOOK_URL')
+#     headers = {"X-Token": X_TOKEN}
+#     payload = {"webHookUrl": WEBHOOK_URL}
+#
+#     app.logger.info(f"Спроба реєстрації вебхука в Monobank на адресу: {WEBHOOK_URL}")
+#     try:
+#         response = requests.post(url, headers=headers, json=payload, timeout=10)
+#         if response.status_code == 200:
+#             app.logger.info("Успішно: Monobank підтвердив реєстрацію вебхука!")
+#         else:
+#             app.logger.error(f"Помилка реєстрації вебхука: {response.status_code} - {response.text}")
+#     except Exception as e:
+#         app.logger.error(f"Не вдалося зв'язатися з API Monobank: {e}")
 
 
 # Запускаємо реєстрацію в окремому потоці, щоб не заважати старту Flask
@@ -70,18 +66,12 @@ def monobank_webhook():
     # 2. ОБРОБКА POST-ЗАПИТУ (Отримання реальних транзакцій)
     if request.method == 'POST':
         try:
-
             raw_data = request.get_data(as_text=True)
-
             if not raw_data:
                 app.logger.warning("Отримано порожній запит POST.")
                 return "Empty body", 400
-
             app.logger.info("Отримано вебхук від Monobank. Запис в MariaDB...")
-
-
             save_json(raw_data)
-
             app.logger.info("Успішно: Транзакцію збережено в raw_webhooks.")
             return "OK", 200
 
--- config.py
+++ config.py
@@ -30,27 +30,21 @@ def get_secret_value(secret_name,env,path):
     return secret.secretValue
 
 params = {
-
+# MariaDB params
     'db_host'        : get_secret_value("DB_HOST", secret_env,"/database"),
     'db_port'        : int(get_secret_value("DB_PORT", secret_env,"/database")),
     'db_user'        : get_secret_value("DB_USER", secret_env,"/database"),
     'db_password'    : get_secret_value("DB_PASSWORD", secret_env,"/database"),
     'db_name'        : get_secret_value("DB_NAME", secret_env,"/database"),
-    'bank_api_url'   : get_secret_value("MONO_API_URL",secret_env,"/bank"),
-    'actual_api_url' : get_secret_value("ACTUAL_API_SERVER_URL",secret_env,"/actual"),
-    'actual_sync_id' : get_secret_value("ACTUAL_SYNC_ID",secret_env,"/actual"),
-    'actual_api_key' : get_secret_value('ACTUAL_API_KEY',secret_env,"/actual"),
-    'cur_rate_url'   : get_secret_value('CUR_RATE_URL',secret_env,"/bank"),
+
+# MONO params
     'bill_webhook_url'   : get_secret_value('BILL_WEBHOOK_URL',secret_env,"/bank"),
-    'bill_webhook_token' : get_secret_value('BILL_WEBHOOK_TOKEN',secret_env,'/bank'),
-    'mono_webhook_url'   : get_secret_value('MONO_WEBHOOK_URL',secret_env,'/bank'),
+
+# sus params
     'debug_mode' : int(os.getenv('DEBUG_MODE',0)),
     'env' : os.getenv('ENV')
 }
 
-
 if params.get('debug_mode') == 1:
     for key,value in params.items():
         print(key,':',value)
-
-
