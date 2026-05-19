b5f78ebdd50274d2c31fcaad66efaa6defb2b84e
Kostevych Viacheslav <tomkat.s@gmail.com>
2026-05-19 09:42:32 +0300

    ref 68

--- config.py
+++ config.py
@@ -36,10 +36,8 @@ params = {
     'db_user'        : get_secret_value("DB_USER", secret_env,"/database"),
     'db_password'    : get_secret_value("DB_PASSWORD", secret_env,"/database"),
     'db_name'        : get_secret_value("DB_NAME", secret_env,"/database"),
-
 # MONO params
     'bill_webhook_url'   : get_secret_value('BILL_WEBHOOK_URL',secret_env,"/bank"),
-
 # sus params
     'debug_mode' : int(os.getenv('DEBUG_MODE',0)),
     'env' : os.getenv('ENV')
